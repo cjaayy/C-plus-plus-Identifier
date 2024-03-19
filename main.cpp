a935126f9deff08d6a21f89ebbdf44554a14b4e9
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2024-03-19 11:02:29 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-#include <istream>
 
 using namespace std;
 
 int main() {
+    
     // Declared variables
     char user_entry = ' ';
 
     // User input
+    cout << endl;
     cout << "Enter Any Character: ";
     cin >> user_entry;
 
     // Check if it's an alphabet letter
-    if ((user_entry >= 'a' && user_entry <= 'z') || (user_entry >= 'A' && user_entry <= 'Z') || (user_entry == 'Ñ') || (user_entry == 'ñ')) {
-        cout << "It's Alphabet!";
+    if ((user_entry >= 'a' && user_entry <= 'z') || (user_entry >= 'A' && user_entry <= 'Z') || user_entry == char(209) && user_entry == (241)) {
+        cout << "It's an Alphabet!";
     }
     // Check if it's a digit
     else if (user_entry >= '0' && user_entry <= '9') {
-        cout << "It's Digit!";
-    }
-    // Other special character
+        cout << "It's a Digit!";
+    } 
     else {
         cout << "It's Special Character!";
     }
